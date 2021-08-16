70ea4329f931f1e55cbcb53642b18370bfec19d3
bzkarim250 <bzkarim250@gmail.com>
2021-08-16 22:27:47 +0200

    Delete online-shop directory

--- online-shop/connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Create connection
-$conn =mysqli_connect('localhost', 'admin','admin','electronics');
-//check connection
-if(!$conn)
-{
-    echo 'connection error:'.mysqli_connect_error();
-}
-?>
--- online-shop/home.php
+++
@@ -1,9 +0,0 @@
-<?php include("topnav.php"); ?>
-<h1> WELCOME TO UNIQUE ELCTRONICS SHOP</h1>
-<center>
-<b> <p style="color:rgb(14, 59, 80);font-size:20px"> We deliver you quality smartphones |  laptops  |electronics accessories </b> </P>
-<div class="slider" > </div> 
-</center>
-<?php include("footer.php"); ?>
-</body>
-</html>
