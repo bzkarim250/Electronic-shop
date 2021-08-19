3e0edb8a2bcda9690bf5fa6776a34fb5119c3f28
bzkarim250 <bzkarim250@gmail.com>
2021-08-19 23:36:37 +0200

    Add files via upload

--- topnav.php
+++ topnav.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="footter.css">
     <script src="https://kit.fontawesome.com/07796c0bdb.js" crossorigin="anonymous"></script>
 <title>
@@ -20,8 +21,39 @@
             <a href="Home.php">Home</a>
             <a  href="product.php">Products</a>
             <a  href="#">Profile</a>
-                     <a href="Login.php">Login</a>
+            
+
+            <?php //hiding signup if user is logged in
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if(!isset($_SESSION["login"]))
+{
+
+?>
+
+<a href="Login.php">Login</a>
+                     <?php
+                     }
+                                          ?>
+
+
+
+            
+                     
+                     <?php //hiding signup if user is logged in
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if(!isset($_SESSION["login"]))
+{
+
+?>
+
                      <a href="register.php">Sign_Up</a>
+                     <?php
+                     }
+                                          ?>
                      <a  href="sms.php">Contact US</a>
                 
     </ul>
